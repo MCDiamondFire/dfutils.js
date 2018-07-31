1e851bd54900886ca3fa77ecd0929f737393b065
RedstoneDaedalus <cosimoplumb@gmail.com>
2018-07-31 16:08:11 -0600

    Slight syntax change

--- structures/Item.js
+++ structures/Item.js
@@ -1,14 +1,26 @@
 const items = require("../items.json");
 
 class Item {
-    constructor(count, id, metadata = {}, damage = 0) {
-        const { type } = items.find(item => item.meta === damage && (item.type === id || item.text_type === id));
+    constructor(id) {
+        const { type } = items.find(item => item.type === id || item.text_type === id);
         if (!type) throw new Error("Unknown item.");
 
-        this.Count = count;
+        this.Count = 1;
         this.id = type;
-        this.tag = metadata;
-        this.Damage = damage;
+        this.tag = { };
+        this.damage = 0
+    }
+
+    setAmounth(count) {
+        this.Count = count;
+    }
+
+    setTag(tag) {
+        this.tag = tag;
+    }
+
+    setDamage(damage) {
+        this.Damage = Damage;
     }
 }
 
