2a8ec0afeb0bc804b19c98e488238f724bd96b4e
RedstoneDaedalus <cosimoplumb@gmail.com>
2018-08-01 10:18:51 -0600

    Corrected lore

--- structures/Value.js
+++ structures/Value.js
@@ -5,7 +5,7 @@ class Value {
         this.tag = {
             HideFlags: 63,
             display: {
-                Lore: ["&7A variable type that cannot be", "&7modified. It changes based on", "&7the statistics of the plot and the", "&7players on it.", "", "&dValues:", "&b> &7Statistic (right click while holding)", "&b> &7Value target (sneak + right click while holding)"],
+                Lore: ["&7A variable type that cannot be", "&7modified. It changes based on", "&7the statistics of the plot and the", "&7players on it.", "", "&dValues:", "&b> &7Statistic (right click while holding)", "&b> &7Value target (sneak + right click", "&7while holding)"],
                 Name: name
             }
         }
